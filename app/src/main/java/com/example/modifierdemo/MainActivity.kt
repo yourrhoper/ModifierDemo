6083144ce90ad9112f3e265ad10354f8097b2c69
yourrhoper <117834124+yourrhoper@users.noreply.github.com>
2026-09-14 23:23:08 +0700

    Delete Greeting

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -30,13 +30,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
